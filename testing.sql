ef0cc757abd5806985df53e5eaae4324a0251328
JaichandranR <highmast@gmail.com>
2024-10-18 18:20:15 -0400

    Update testing.sql

--- testing.sql
+++ testing.sql
@@ -9,8 +9,8 @@ WITH (
   api_additional_headers = '{}',
   api_request_retry_settings = '{"retry":false,"attempts":5,"delaySeconds":15}',
   api_response_header_accept = 'application/json',
-  -- Extracting the applicationId and knownApplicationProductionPlatforms fields in one simple call
-  jsonpath_record_selector = '$.applications[*].architecture.knownApplicationProductionPlatforms[*]',
+  -- Ensure that applicationId is extracted together with knownApplicationProductionPlatforms fields
+  jsonpath_record_selector = '$.applications[*].{applicationId:applicationId, platformCode:architecture.knownApplicationProductionPlatforms[*].platformCode, sourceApplicationId:architecture.knownApplicationProductionPlatforms[*].sourceApplicationId, associatedDeploymentId:architecture.knownApplicationProductionPlatforms[*].associatedDeploymentId, lastDiscoveredDateTimeUTC:architecture.knownApplicationProductionPlatforms[*].lastDiscoveredDateTimeUTC}',
   request_method = 'GET',
   return_format = 'JSON',
   target_uri_pattern = 'https://seal-api.gaiacloud.jpmchase.net/applications'
