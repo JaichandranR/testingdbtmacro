d16bc2b508c2befc708fa533bae4bccb2234e8a2
JaichandranR <highmast@gmail.com>
2024-10-18 18:22:33 -0400

    Update testing.sql

--- testing.sql
+++ testing.sql
@@ -1,16 +1,13 @@
 CREATE TABLE IF NOT EXISTS apitables_prototype.seal_prototype.application_platform (
   applicationId varchar,
-  platformCode varchar,
-  sourceApplicationId varchar,
-  associatedDeploymentId varchar,
-  lastDiscoveredDateTimeUTC varchar
+  platforms varchar -- Store the platforms array as a string
 )
 WITH (
   api_additional_headers = '{}',
   api_request_retry_settings = '{"retry":false,"attempts":5,"delaySeconds":15}',
   api_response_header_accept = 'application/json',
-  -- Ensure that applicationId is extracted together with knownApplicationProductionPlatforms fields
-  jsonpath_record_selector = '$.applications[*].{applicationId:applicationId, platformCode:architecture.knownApplicationProductionPlatforms[*].platformCode, sourceApplicationId:architecture.knownApplicationProductionPlatforms[*].sourceApplicationId, associatedDeploymentId:architecture.knownApplicationProductionPlatforms[*].associatedDeploymentId, lastDiscoveredDateTimeUTC:architecture.knownApplicationProductionPlatforms[*].lastDiscoveredDateTimeUTC}',
+  -- Extract applicationId and store the platforms array as a string
+  jsonpath_record_selector = '$.applications[*].{applicationId:applicationId, platforms:architecture.knownApplicationProductionPlatforms}',
   request_method = 'GET',
   return_format = 'JSON',
   target_uri_pattern = 'https://seal-api.gaiacloud.jpmchase.net/applications'
