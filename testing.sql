c41baf8f095aa61224fc84a38211fb6183814a4e
JaichandranR <highmast@gmail.com>
2024-10-18 18:28:07 -0400

    Update testing.sql

--- testing.sql
+++ testing.sql
@@ -9,8 +9,8 @@ WITH (
   api_additional_headers = '{}',
   api_request_retry_settings = '{"retry":false,"attempts":5,"delaySeconds":15}',
   api_response_header_accept = 'application/json',
-  -- Embed applicationId within each platform entry to ensure all fields are extracted
-  jsonpath_record_selector = '$.applications[*].architecture.knownApplicationProductionPlatforms[*].{applicationId:$.applications[*].applicationId, platformCode:platformCode, sourceApplicationId:sourceApplicationId, associatedDeploymentId:associatedDeploymentId, lastDiscoveredDateTimeUTC:lastDiscoveredDateTimeUTC}',
+  -- Extract knownApplicationProductionPlatforms fields and manually include applicationId with a cleaner approach
+  jsonpath_record_selector = '$.applications[*].{applicationId:applicationId, platformCode:architecture.knownApplicationProductionPlatforms[*].platformCode, sourceApplicationId:architecture.knownApplicationProductionPlatforms[*].sourceApplicationId, associatedDeploymentId:architecture.knownApplicationProductionPlatforms[*].associatedDeploymentId, lastDiscoveredDateTimeUTC:architecture.knownApplicationProductionPlatforms[*].lastDiscoveredDateTimeUTC}',
   request_method = 'GET',
   return_format = 'JSON',
   target_uri_pattern = 'https://seal-api.gaiacloud.jpmchase.net/applications'
