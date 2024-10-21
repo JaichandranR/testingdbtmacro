3f931ab8fb1025b60fe85d7996972158dfc82017
JaichandranR <highmast@gmail.com>
2024-10-20 22:00:17 -0400

    Update testing.sql

--- testing.sql
+++ testing.sql
@@ -1,22 +1,15 @@
-{{ config(
-    materialized='table',
-    file_format='PARQUET',
-    on_table_exists='drop'
-) }}
-
 WITH flattened_data AS (
     SELECT
         -- Extract applicationId
         json_extract_scalar(raw, '$.applicationId') AS applicationId,
         -- Unnest knownApplicationProductionPlatforms and extract fields
-        json_extract_scalar(platform.value, '$.platformCode') AS platformCode,
-        json_extract_scalar(platform.value, '$.sourceApplicationId') AS sourceApplicationId,
-        json_extract_scalar(platform.value, '$.lastDiscoveredDateTimeUTC') AS lastDiscoveredDateTimeUTC
+        json_extract_scalar(platform, '$.platformCode') AS platformCode,
+        json_extract_scalar(platform, '$.sourceApplicationId') AS sourceApplicationId,
+        json_extract_scalar(platform, '$.lastDiscoveredDateTimeUTC') AS lastDiscoveredDateTimeUTC
     FROM {{ ref('raw_32010_application') }},
-    -- Use CROSS JOIN UNNEST to unnest the platforms array
+    -- Correct UNNEST syntax, no need for `.value`
     UNNEST(json_parse(json_extract(raw, '$.architecture.knownApplicationProductionPlatforms'))) AS platform
 )
 
 SELECT *
 FROM flattened_data;
-
